811aff14d341566480baeb105a80514b5c9f37ee
Luis Giraldo <desarrolloingenieria13@udea.edu.co>
2023-10-28 19:29:24 -0500

    refactor: index and private route HOC

---
+++ HOC/PrivateRoute.tsx
@@ -0,0 +1,24 @@
+import { LoadingOverlay } from '@/components/Loading/LoadingOverlay';
+import { Login } from '@/components/Login';
+import { useSession } from 'next-auth/react';
+import { ComponentType, FC } from 'react';
+
+export const withPrivateRoute = <Props extends object>(
+  PrivateComponent: ComponentType<Props>
+) => {
+  const ProtectedComponent: FC<Props> = (props) => {
+    const { status } = useSession();
+
+    if (status === 'loading') {
+      return <LoadingOverlay />;
+    }
+
+    if (status === 'unauthenticated') {
+      return <Login />;
+    }
+
+    return <PrivateComponent {...props} />;
+  };
+
+  return ProtectedComponent;
+};
