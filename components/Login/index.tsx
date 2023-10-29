c0b8fc151c46a9c47c7aa5bb8d0f0166b982da78
Luis Giraldo <desarrolloingenieria13@udea.edu.co>
2023-10-28 19:28:59 -0500

    feat: login component

---
+++ components/Login/index.tsx
@@ -0,0 +1,12 @@
+import { signIn } from 'next-auth/react';
+import { FC } from 'react';
+
+export const Login: FC = () => {
+  return (
+    <main className='flex flex-col gap-10 items-center justify-center min-h-svh'>
+      <h1 className='text-4xl'>Sistema de gestión de inventarios</h1>
+
+      <button onClick={() => signIn('auth0')}>Iniciar Sesión</button>
+    </main>
+  );
+};
