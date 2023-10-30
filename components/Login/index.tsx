e136074b95799b9d09a2b95aeb78040455e6c9a7
Luis Giraldo <desarrolloingenieria13@udea.edu.co>
2023-10-29 21:25:18 -0500

    fix: pages arrange

--- components/Login/index.tsx
+++ components/Login/index.tsx
@@ -3,7 +3,7 @@ import { FC } from 'react';
 
 export const Login: FC = () => {
   return (
-    <main className='flex flex-col gap-10 items-center justify-center min-h-svh'>
+    <main className='flex flex-col gap-10 items-center justify-center w-full'>
       <h1 className='text-4xl'>Sistema de gestión de inventarios</h1>
 
       <button onClick={() => signIn('auth0')}>Iniciar Sesión</button>
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,10 +1,11 @@
 import { withPrivateRoute } from '@/HOC/PrivateRoute';
+import { BaseLayout } from '@/layout/BaseLayout';
 
 const Home = () => {
   return (
-    <main className='h-svh w-full flex items-center justify-center'>
-      Hola mundo
-    </main>
+    <BaseLayout>
+      <p className='w-full flex items-center justify-center'>Hola mundo</p>
+    </BaseLayout>
   );
 };
 
