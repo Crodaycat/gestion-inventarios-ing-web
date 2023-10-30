aad6c905384c1b2b6901e521a41fb64405be9068
Luis Giraldo <desarrolloingenieria13@udea.edu.co>
2023-10-29 21:26:09 -0500

    feat: users page table

---
+++ pages/usuarios.tsx
@@ -0,0 +1,71 @@
+import { withPrivateRoute } from '@/HOC/PrivateRoute';
+import { Loading } from '@/components/Loading';
+import { Paginator } from '@/components/Pagination';
+import { useGetUsers } from '@/hooks/useGetUsers';
+import { BaseLayout } from '@/layout/BaseLayout';
+import Image from 'next/image';
+import { useState } from 'react';
+
+const Home = () => {
+  const { users, totalCount, usersLoading } = useGetUsers();
+  const [page, setPage] = useState<number>(1);
+
+  return (
+    <BaseLayout>
+      <section className='w-full flex flex-col items-center p-4 gap-5'>
+        <h1 className='text-4xl'>Gestión de Usuarios</h1>
+
+        <table cellSpacing='0'>
+          <thead>
+            <tr>
+              <th>Imagen</th>
+              <th>Nombre</th>
+              <th>Correo</th>
+              <th>Rol</th>
+              <th>Acciones</th>
+            </tr>
+          </thead>
+          <tbody>
+            {usersLoading && (
+              <tr>
+                <td colSpan={5}>
+                  <div className='flex justify-center items-center'>
+                    <Loading /> <span>Cargando...</span>
+                  </div>
+                </td>
+              </tr>
+            )}
+
+            {users?.map((user) => {
+              return (
+                <tr key={user.id}>
+                  <td>
+                    <Image
+                      src={user?.image ?? '/media/default-user.jpg'}
+                      width={40}
+                      height={40}
+                      alt='User image'
+                      className='rounded-full'
+                    />
+                  </td>
+                  <td>{user.name}</td>
+                  <td>{user.email}</td>
+                  <td></td>
+                  <td></td>
+                </tr>
+              );
+            })}
+          </tbody>
+        </table>
+        <Paginator
+          currentPage={page}
+          itemsPerPage={20}
+          totalCount={totalCount}
+          updatePage={setPage}
+        />
+      </section>
+    </BaseLayout>
+  );
+};
+
+export default withPrivateRoute(Home);
