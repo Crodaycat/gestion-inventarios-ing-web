9317b90f6d2c2cdc822b2a6fe5b127e983a12b9a
Luis Giraldo <desarrolloingenieria13@udea.edu.co>
2023-10-29 21:30:46 -0500

    feat: add on click to button component

--- components/Button/index.tsx
+++ components/Button/index.tsx
@@ -11,15 +11,18 @@ export interface ButtonProps {
   color?: Color;
   size?: Size;
   children?: ReactNode;
+  onClick?: () => void;
 }
 
 export const Button: FC<ButtonProps> = ({
   children,
   color = 'normal',
   size = 'normal',
+  onClick,
 }) => (
   <button
     className={`cursor-pointer text-lg py-1.5 px-3 rounded-sm ${BackgroundColors[color]} ${TextColors[color]} ${TextSizes[size]}`}
+    onClick={onClick}
   >
     {children}
   </button>
