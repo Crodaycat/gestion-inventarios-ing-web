9d3a90b817d23aa5ad4a6ad856288d6bc672cf45
Luis Giraldo <desarrolloingenieria13@udea.edu.co>
2023-10-29 21:30:58 -0500

    fix: loading overlay component

--- components/Loading/LoadingOverlay.tsx
+++ components/Loading/LoadingOverlay.tsx
@@ -2,7 +2,7 @@ import { FC } from 'react';
 import { Loading } from '.';
 
 export const LoadingOverlay: FC = () => (
-  <div className='flex items-center justify-center min-h-svh text-2xl'>
+  <div className='flex w-full items-center justify-center min-h-svh text-2xl'>
     <Loading size={2} />
 
     <span>Cargando...</span>
