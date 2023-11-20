36f1a3e12b4e6f3e5d41185a7f8bcba90ceaa463
Gabriel Camargo <gabriel.camargo@udea.edu.co>
2023-11-19 22:25:47 -0500

    Add style to disabled button

--- pages/inventarios.tsx
+++ pages/inventarios.tsx
@@ -69,6 +69,7 @@ const Home = () => {
             color='primary'
             size='extraLarge'
             onClick={() => { setOpenNewMovement(true) }}
+            disabled = {!selectedMaterial?.id}
           >
             Agregar movimiento
           </Button>
