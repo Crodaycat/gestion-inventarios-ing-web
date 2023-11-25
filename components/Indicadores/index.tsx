c507e4c428a06f8ca3f9632c466b01399263c19d
esn8der <ballesta.esneider@gmail.com>
2023-11-25 00:19:34 -0500

    Refactor Indicadores component to improve readability and performance

--- components/Indicadores/index.tsx
+++ components/Indicadores/index.tsx
@@ -1,94 +1,65 @@
-import React from 'react';
-import {
-  Chart,
-  CommonSeriesSettings,
-  Series,
-  Point,
-  Legend,
-  Tooltip,
-} from 'devextreme-react/chart';
 import { Movement } from '@/types';
+import Chart, {
+  Series,
+  Legend,
+  CommonSeriesSettings,
+  Margin,
+  ArgumentAxis,
+  Export,
+  Tooltip,
+  Title,
+  Subtitle,
+  Grid,
+} from 'devextreme-react/chart';
 
 interface IndicadoresProps {
   movements: Movement[];
 }
 
 const Indicadores = ({ movements }: IndicadoresProps) => {
-  const grossProductData: { fecha: string; saldo: number }[] = [];
-  let saldoAcumulado = 0;
-  let secondDate = new Date().toLocaleDateString();
+  const formattedMovements = movements.map(movement => ({
+    ...movement,
+    createdAt: new Date(movement.createdAt).toLocaleDateString(),
+  }));
 
-  if (Array.isArray(movements)) {
-    movements.reverse().forEach((movement, index, array) => {
-      {
-        movement.movementType === 'ENTRADA'
-          ? (saldoAcumulado += movement.quantity)
-          : (saldoAcumulado -= movement.quantity);
-      }
-      const firstDate = new Date(movement.createdAt).toLocaleDateString();
+  const groupedMovements = formattedMovements.reduce((acc, { createdAt, movementType, quantity }) => {
+    acc[createdAt] ||= { date: createdAt, quantity: 0 };
+    acc[createdAt].quantity += (movementType === 'ENTRADA' ? 1 : -1) * quantity;
+    return acc;
+  }, {} as Record<string, { date: string; quantity: number }>);
 
-      if (secondDate === null) {
-        secondDate = firstDate;
-      }
+  const chartData = Object.values(groupedMovements);
 
-      if (secondDate !== firstDate) {
-        grossProductData.push({
-          fecha: secondDate,
-          saldo: saldoAcumulado,
-        });
-        secondDate = new Date().toLocaleDateString();
-      }
+  const totalBalance = chartData.reduce((sum, { quantity }) => sum + quantity, 0);
 
-      if (index === array.length - 1 && array.length > 1) {
-        if (grossProductData.length === 0) {
-          grossProductData.push({
-            fecha: new Date(movement.createdAt).toLocaleDateString(),
-            saldo: saldoAcumulado,
-          });
-        } else {
-          grossProductData.push({
-            fecha: new Date(movement.createdAt).toLocaleDateString(),
-            saldo: saldoAcumulado - movement.quantity,
-          });
-        }
-      }
-
-      if (array.length === 1) {
-        grossProductData.push({
-          fecha: new Date(movement.createdAt).toLocaleDateString(),
-          saldo: saldoAcumulado,
-        });
-      }
-    });
-  }
-  const title = `Saldo: ${saldoAcumulado}`;
+  const title = `Saldo total: ${totalBalance}`;
 
   return (
     <div className='flex flex-col w-3/4'>
       <div className='font-semibold text-2xl p-2 text-slate-500'>{title}</div>
-      <section>
-        <Chart
-          id='chart'
-          dataSource={grossProductData.reverse()}
-          stickyHovering={false}
-        >
-          <CommonSeriesSettings
-            argumentField='fecha'
-            type='line'
-            hoverMode='includePoints'
-            line={{ width: 2, color: 'blue' }}
-          >
-            <Point hoverMode='allArgumentPoints' />
-          </CommonSeriesSettings>
-          <Series name='Saldo' valueField='saldo' />
-          <Legend
-            verticalAlignment='bottom'
-            horizontalAlignment='center'
-            hoverMode='excludePoints'
-          />
-          <Tooltip enabled={true} />
-        </Chart>
-      </section>
+      <Chart
+        id='chart'
+        dataSource={chartData.reverse()}
+        title='Evolución de los saldos diarios totales'
+      >
+        <CommonSeriesSettings argumentField='date' type='line' />
+        <Series valueField='quantity' name='Saldo' />
+        <Margin bottom={20} />
+        <ArgumentAxis
+          argumentType='string'
+          grid={{ visible: true }}
+        />
+        <Legend
+          verticalAlignment='bottom'
+          horizontalAlignment='center'
+          itemTextPosition='bottom'
+        />
+        <Export enabled={true} />
+        <Tooltip enabled={true} />
+        <Title text='Evolución de los saldos diarios totales' />
+        <Subtitle text='Material seleccionado' />
+        <Grid visible={true} />
+      </Chart>
     </div>
   );
 };
